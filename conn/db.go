b5da5a81f6a46f7903afc1cfa293dc649015540d
Anik Sarker <aniksarker@pathao.com>
2018-10-24 17:35:37 +0600

    fix models

--- conn/db.go
+++ conn/db.go
@@ -1,1 +1,21 @@
 package conn
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func main() {
+	db, err := gorm.Open("postgres", "host=http://192.168.1.153 port=5432 user=postgres dbname=konta_db password=secret")
+	if err != nil {
+		println(err.Error())
+		println("errir")
+		return
+	}
+	println("success")
+	defer db.Close()
+}
+
+func init() {
+	main()
+}
