86bbb81cd47d2e89a3c41f6bfd2aadf5b8cc8f18
Anik Sarker <aniksarker@pathao.com>
2018-10-29 16:17:52 +0600

    add config file and viper package

---
+++ config/cfg.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+
+var AppConfig *viper.Viper
+
+func LoadConfig()  {
+	v := viper.New()
+	v.SetConfigName("config.konta") // name of config file (without extension)
+	v.AddConfigPath(".")
+	err := v.ReadInConfig() // Find and read the config file
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	AppConfig = v
+}
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/khanadnanxyz/konta/config"
 	"net/http"
 )
 
 func main() {
+	config.LoadConfig()
+	println(config.AppConfig.GetString("postgres.host"))
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
