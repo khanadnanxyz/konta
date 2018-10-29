75a56df5df6cd8edffc741d47b35d8eb20ea9aae
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2018-10-29 16:53:27 +0600

    Adds go-chi

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/khanadnanxyz/konta/config"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
 func main() {
-	config.LoadConfig()
-	println(config.AppConfig.GetString("postgres.host"))
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	//config.LoadConfig()
+	//println(config.AppConfig.GetString("postgres.host"))
+	//http.HandleFunc("/", index)
+	//http.ListenAndServe(":8080", nil)
+	netwrok()
+}
+
+func netwrok() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	http.ListenAndServe(":8080", r)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
