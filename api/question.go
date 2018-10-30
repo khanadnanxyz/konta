494cd4c4394c425a8f7d2cea00562de3abed0cf7
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2018-10-30 13:00:29 +0600

    Does some shitty copy-paste

---
+++ api/question.go
@@ -0,0 +1,9 @@
+package api
+
+import "net/http"
+
+func AllQuestion(w http.ResponseWriter, r *http.Request) {}
+func DetailQuestion(w http.ResponseWriter, r *http.Request) {}
+func CreateQuestion(w http.ResponseWriter, r *http.Request) {}
+func UpdateQuestion(w http.ResponseWriter, r *http.Request) {}
+func DeleteQuestion(w http.ResponseWriter, r *http.Request) {}
--- conn/db.go
+++ conn/db.go
@@ -3,16 +3,9 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/khanadnanxyz/konta/model"
-	_ "github.com/lib/pq"
 )
 
-var (
-	// Models describe models list for migration
-	Models []interface{}
-)
-
-func main() {
+func connect() {
 	db, err := gorm.Open("postgres", "host=192.168.1.153 port=5432 user=postgres dbname=konta_db password=secret sslmode=disable")
 	if err != nil {
 		println(err.Error())
@@ -21,22 +14,4 @@ func main() {
 	}
 	println("success")
 	defer db.Close()
-
-	// add address model for migration
-	Models = append(Models, &model.Role{})
-	Models = append(Models, &model.User{})
-	Models = append(Models, &model.Category{})
-	Models = append(Models, &model.Question{})
-	Models = append(Models, &model.Option{})
-	Models = append(Models, &model.Answer{})
-
-	//doesn't work
-	//db.Model(&model.User{}).AddForeignKey("role_id", "roles(id)", "RESTRICT", "RESTRICT")
-
-	db.AutoMigrate(Models...)
-
-}
-
-func init() {
-	main()
 }
